be45e2a93b3071a73ffc111e5a8d994e48ac17a1
NotAWiz4rd <xamasa98@gmail.com>
2017-03-27 01:57:49 +0200

    SAI-1: replaced java-icon and added debugPanelControl

--- src/GUI/GuiManager.java
+++ src/GUI/GuiManager.java
@@ -1,7 +1,12 @@
 package GUI;
 
+import Entities.EntityManger;
+
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
 
 /**
  * Created by notawiz4rd on 26/03/2017.
@@ -11,21 +16,54 @@ import javax.swing.WindowConstants;
 public class GuiManager
 {
   private JFrame mainWindow;
+  private JFrame debugPanel;
   private String version;
+  private EntityManger entityManger = new EntityManger();
+  private Image iconImage;
 
   public GuiManager(String m_version)
   {
     version = m_version;
+    getIconImage();
     initializeMainWindow();
+    initializeDebugPanel();
   }
 
   private void initializeMainWindow()
   {
-    mainWindow = new MainFrame();
+    mainWindow = new MainFrame(entityManger);
     mainWindow.setTitle("MainWindow v" + version);
-    mainWindow.setSize(500, 500);
+    mainWindow.setSize(1000, 1000);
     mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    mainWindow.setResizable(true);
     mainWindow.setVisible(true);
+
+    if(iconImage != null)
+    {
+      mainWindow.setIconImage(iconImage);
+    }
+  }
+
+  private void initializeDebugPanel()
+  {
+    debugPanel = new DebugPanel(entityManger);
+    debugPanel.setTitle("DebugPanel v" + version);
+    debugPanel.setSize(500, 1000);
+    debugPanel.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+    mainWindow.setResizable(false);
+    debugPanel.setVisible(true);
+
+    if(iconImage != null)
+    {
+      debugPanel.setIconImage(iconImage);
+    }
+  }
+
+  private void getIconImage()
+  {
+    URL url = ClassLoader.getSystemResource("sai-icon.png");
+    Toolkit kit = Toolkit.getDefaultToolkit();
+    iconImage = kit.createImage(url);
   }
 
 }
