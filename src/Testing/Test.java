84218e30c2433047bb5291e9feef2365b9e477bd
NotAWiz4rd <xamasa98@gmail.com>
2017-03-26 22:50:25 +0200

    SAI-1: added Testing-Module modified .gitignore to include output

---
+++ src/Testing/Test.java
@@ -0,0 +1,22 @@
+package Testing;
+
+/**
+ * Created by NotAWiz4rd on 26.03.2017.
+ */
+public class Test
+{
+  public Test()
+  {
+    createDatabase();
+  }
+
+  private void createDatabase()
+  {
+    // TODO create testEntities here
+  }
+
+  private void createEnvironment()
+  {
+    // TODO create a Test-Environment here
+  }
+}
